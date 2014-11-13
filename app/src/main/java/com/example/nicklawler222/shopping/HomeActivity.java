5214c4f53644666f9135af63dd9dc6e43e6894a8
denver <denver.chen@gmail.com>
2014-11-12 17:58:45 -0800

    Merge branch 'initialUI' of https://github.com/nicklawls/Shopping
    Conflicts:
    	app/app.iml
    	app/build.gradle
    	app/src/main/AndroidManifest.xml
    	app/src/main/java/com/example/nicklawler222/shopping/MyActivity.java
    	app/src/main/res/layout/activity_my.xml
    	app/src/main/res/menu/my.xml
    	app/src/main/res/values/strings.xml
    	app/src/main/res/values/strings_activity_login.xml

--- app/src/main/java/com/example/nicklawler222/shopping/HomeActivity.java
+++ app/src/main/java/com/example/nicklawler222/shopping/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.nicklawler222.shopping;
 
 import android.app.Activity;
+import java.sql.*;
+import java.util.ArrayList;
 
 import android.app.ActionBar;
 import android.app.Fragment;
@@ -8,6 +10,7 @@ import android.app.FragmentManager;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.AsyncTask;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -20,7 +23,7 @@ import android.widget.TextView;
 
 
 public class HomeActivity extends Activity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks,CategoryFragment.OnFragmentInteractionListener {
 
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
@@ -31,7 +34,7 @@ public class HomeActivity extends Activity
      * Used to store the last screen title. For use in {@link #restoreActionBar()}.
      */
     private CharSequence mTitle;
-
+    public void onFragmentInteraction(String id) {}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,10 +53,17 @@ public class HomeActivity extends Activity
     @Override
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing fragments
+        ArrayList productnumbers = new ArrayList();
+        ArrayList productnames = new ArrayList();
+        productnumbers.add("1");
+        productnumbers.add("2");
+        productnames.add("Shoes");
+        productnames.add("Ford Mustang");
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .replace(R.id.container, CategoryFragment.newInstance(productnumbers,productnames))
                 .commit();
+
     }
 
     public void onSectionAttached(int number) {
@@ -106,10 +116,17 @@ public class HomeActivity extends Activity
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+    public static class product_fragment extends Fragment {
+    //private WeakReference<FetchSQL> asyncTaskWeakRef;
+
+
+    TextView productno;
+    TextView productname;
+    TextView productcategory;
+    TextView productprice;
+    TextView productdescription;
+    TextView productfeatures;
+
         /**
          * The fragment argument representing the section number for this
          * fragment.
@@ -120,23 +137,99 @@ public class HomeActivity extends Activity
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
+        public static product_fragment newInstance(int sectionNumber,int productNumber) {
+            product_fragment fragment = new product_fragment();
             Bundle args = new Bundle();
             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            args.putInt("product_number",productNumber);
             fragment.setArguments(args);
             return fragment;
         }
 
-        public PlaceholderFragment() {
+        public product_fragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_home2, container, false);
+            Bundle args = getArguments();
+            int productnumber = args.getInt("product_number",0);
+
+
+
+            View rootView = inflater.inflate(R.layout.product_fragment, container, false);
+            productno = (TextView) rootView.findViewById(R.id.productno);
+            productno.setText(Integer.toString(productnumber));
+
+            productname = (TextView) rootView.findViewById(R.id.productname);
+
+            productcategory = (TextView) rootView.findViewById(R.id.productcategory);
+
+            productprice = (TextView) rootView.findViewById(R.id.productprice);
+
+            productdescription = (TextView) rootView.findViewById(R.id.productdescription);
+
+            productfeatures = (TextView) rootView.findViewById(R.id.productfeatures);
+            new FetchSQL().execute(productnumber);
+
+
+
             return rootView;
         }
+
+        private class FetchSQL extends AsyncTask <Integer,Void,Bundle> {
+            protected Bundle doInBackground(Integer... productnumbers) {
+                try {
+                    Class.forName("org.postgresql.Driver");
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                Bundle product = new Bundle();
+                String url;
+                url = "jdbc:postgresql://shopandgodb.cv80ayxyiqrh.us-west-2.rds.amazonaws.com:5432/sagdb?user=shopandgo&password=goandshop";
+                Connection conn;
+                try {
+                    DriverManager.setLoginTimeout(5);
+                    conn = DriverManager.getConnection(url);
+                    Statement st = conn.createStatement();
+                    String sql;
+                    sql = "SELECT * FROM products WHERE product_no = '" + productnumbers[0] + "'";
+                    ResultSet rs = st.executeQuery(sql);
+                    while (rs.next()) {
+                        product.putString("product_name", rs.getString("name"));
+                        product.putString("product_category", rs.getString("category"));
+                        product.putString("product_price", rs.getString("price"));
+                        product.putString("product_description", rs.getString("description"));
+                        product.putString("product_features", rs.getString("features"));
+                        product.putString("product_imgurl", rs.getString("img_url"));
+                    }
+
+                    rs.close();
+                    st.close();
+                    conn.close();
+                    return product;
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+                protected void onPostExecute(Bundle product) {
+                    productname.setText(product.getString("product_name",""));
+                    productcategory.setText(product.getString("product_category",""));
+                    productprice.setText(product.getString("product_price",""));
+                    productdescription.setText(product.getString("product_description",""));
+                    productfeatures.setText(product.getString("product_features",""));
+
+                }
+
+
+            }
+
+
+
+
+
 
         @Override
         public void onAttach(Activity activity) {
