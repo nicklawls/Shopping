65f88528756ca36a64e596620fb76231eacd11c0
denver <denver.chen@gmail.com>
2014-11-13 18:26:55 -0800

    browse history working
    todo: search button // search history

--- app/src/main/java/com/example/nicklawler222/shopping/DataHolder.java
+++ app/src/main/java/com/example/nicklawler222/shopping/DataHolder.java
@@ -1,12 +1,17 @@
 package com.example.nicklawler222.shopping;
 
+import java.util.ArrayList;
+
 /**
  * Created by Eric on 11/13/2014.
  */
 public class DataHolder {
     private String data_username;
+    private ArrayList searchHistory;
     public String getData() { return data_username; }
+    public ArrayList getHistory() {return searchHistory; }
     public void setData(String data) { this.data_username = data; }
+    public void addHistory(String text) {searchHistory.add(text);}
 
     private static final DataHolder holder = new DataHolder();
     public static DataHolder getInstance() {return holder;}
---
+++ app/src/main/java/com/example/nicklawler222/shopping/SearchListFragment.java
@@ -0,0 +1,150 @@
+package com.example.nicklawler222.shopping;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A fragment representing a list of Items.
+ * <p />
+ * Large screen devices (such as tablets) are supported by replacing the ListView
+ * with a GridView.
+ * <p />
+ * Activities containing this fragment MUST implement the {@link Callbacks}
+ * interface.
+ */
+public class SearchListFragment extends Fragment implements AbsListView.OnItemClickListener {
+    private List SearchListItemList;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    /**
+     * The fragment's ListView/GridView.
+     */
+    private AbsListView mListView;
+
+    /**
+     * The Adapter which will be used to populate the ListView/GridView with
+     * Views.
+     */
+    private ListAdapter mAdapter;
+
+    public static SearchListFragment newInstance(ArrayList searchHistory) {
+        SearchListFragment fragment = new SearchListFragment();
+        Bundle args = new Bundle();
+        args.putStringArrayList("search_history",searchHistory);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public SearchListFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        ArrayList searchHistory = args.getStringArrayList("search_history");
+        SearchListItemList = new ArrayList();
+        for (int i = 0; i < searchHistory.size(); i++) {
+            SearchListItemList.add(new SearchListItem(searchHistory.get(i).toString()));
+        }
+        mAdapter = new SearchListAdapter(getActivity(), SearchListItemList);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_category, container, false);
+
+        // Set the adapter
+        mListView = (AbsListView) view.findViewById(android.R.id.list);
+        ((AdapterView<ListAdapter>) mListView).setAdapter(mAdapter);
+
+        // Set OnItemClickListener so we can be notified on item clicks
+        mListView.setOnItemClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        /*try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                + " must implement OnFragmentInteractionListener");
+        }*/
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+    @Override public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        SearchListItem item = (SearchListItem) this.SearchListItemList.get(position);
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction ft = manager.beginTransaction();
+        //ft.replace(R.id.frame_container,ProductFragment.newInstance(item.getSearchText().toString()));
+        //redo search if they click on search history text
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+
+    /**
+     * The default content for this Fragment has a TextView that is shown when
+     * the list is empty. If you would like to change the text, call this method
+     * to supply the text it should use.
+     */
+    public void setEmptyText(CharSequence emptyText) {
+        View emptyView = mListView.getEmptyView();
+
+        if (emptyText instanceof TextView) {
+            ((TextView) emptyView).setText(emptyText);
+        }
+    }
+
+    /**
+    * This interface must be implemented by activities that contain this
+    * fragment to allow an interaction in this fragment to be communicated
+    * to the activity and potentially other fragments contained in that
+    * activity.
+    * <p>
+    * See the Android Training lesson <a href=
+    * "http://developer.android.com/training/basics/fragments/communicating.html"
+    * >Communicating with Other Fragments</a> for more information.
+    */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(String id);
+    }
+
+}
