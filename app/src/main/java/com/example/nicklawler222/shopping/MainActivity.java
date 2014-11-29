79a5e6ae546a80629ecbcfac28d9b82dcfb58722
Anagaby Toledo-Luna <atole005@ucr.edu>
2014-11-28 22:40:58 -0800

    purchase form done

--- app/src/main/java/com/example/nicklawler222/shopping/MainActivity.java
+++ app/src/main/java/com/example/nicklawler222/shopping/MainActivity.java
@@ -248,6 +248,10 @@ public class MainActivity extends Activity {
                 fragment = new SearchHistoryFragment();
                 break;
             case 9:
+                Intent temp = new Intent(MainActivity.this, PurchaseActivity.class);
+                startActivity(temp);
+                break;
+            case 10:
                 if (DataHolder.getInstance().getData() == "default") { // if no one's logged in
                     Intent i = new Intent(MainActivity.this, LoginActivity.class);
                     startActivity(i);
@@ -258,7 +262,7 @@ public class MainActivity extends Activity {
                 }
 
                 break;
-            case 10:
+            case 11:
                 if (DataHolder.getInstance().getData() != "default") { // if logged in
                     DataHolder.getInstance().setData("default"); // log that nigga out
                     // only changing the intent to change the name at the top, kind of a hack...
