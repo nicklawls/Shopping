ed1aa31b688caf2bf9b762fe31977139eb1d66b4
yhern006 <yhern006@ucr.edu>
2014-12-01 17:36:07 -0800

    This is the "List of Ratings and Reviews" implementation. RateReviewListFragment has yet to be completed. I commented out the stuff that made the app crash.

--- app/src/main/java/com/example/nicklawler222/shopping/RateReviewListAdapter.java
+++ app/src/main/java/com/example/nicklawler222/shopping/RateReviewListAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.example.nicklawler222.shopping.R;
@@ -27,6 +28,8 @@ public class RateReviewListAdapter extends ArrayAdapter{
         TextView userText;
         TextView ratingText;
         TextView reviewText;
+        RatingBar listRating;
+        float float_list_rating;
     } /** * * @param position * @param convertView * @param parent * @return */
 
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -36,27 +39,31 @@ public class RateReviewListAdapter extends ArrayAdapter{
         View viewToUse = null; // This block exists to inflate the settings list item conditionally based on whether // we want to support a grid or list view.
         LayoutInflater mInflater = (LayoutInflater) context .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
         if (convertView == null) {
-//            if(useList){
-//                viewToUse = mInflater.inflate(R.layout.fragment_rate_review_list, null);
-//            }
-//            else {
                 viewToUse = mInflater.inflate(R.layout.fragment_rate_review_list, null);
-            //}
             holder = new ViewHolder();
 
             // userTextView, ratingTextView & reviewTextView are ids from the fragment_rate_review_list xml
             holder.userText = (TextView)viewToUse.findViewById(R.id.userTextView);
             holder.ratingText = (TextView)viewToUse.findViewById(R.id.ratingTextView);
             holder.reviewText = (TextView)viewToUse.findViewById(R.id.reviewTextView);
+            holder.listRating = (RatingBar)viewToUse.findViewById(R.id.user_rating);
 
             viewToUse.setTag(holder);
         }
         else {
             viewToUse = convertView; holder = (ViewHolder) viewToUse.getTag();
         }
+
         holder.userText.setText(item.getUserText());
         holder.ratingText.setText(item.getRatingText());
         holder.reviewText.setText(item.getReviewText());
+
+        // Deals with Displaying User's Rating Stars
+        if(holder.ratingText.getText().toString() == "0.0")
+            holder.listRating.setRating(0);
+        else
+            holder.listRating.setRating(Float.valueOf(holder.ratingText.getText().toString()));
+        //holder.float_list_rating = holder.listRating.getRating();
 
         return viewToUse;
     }
